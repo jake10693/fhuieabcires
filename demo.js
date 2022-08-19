739dec6995b916d991b990790273c5ad0cfc4bb4
Jacob Carver <jaydevvs16@gmail.com>
2022-08-19 01:32:40 -0700

    this is a message

---
+++ demo.js
@@ -0,0 +1,35 @@
+class createObject{
+    constructor(p1, p2, p3, tomatoes ){
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+    this.tomatoes = tomatoes
+    }
+
+    classFunc(){
+       return "stuff"
+    }
+}
+
+
+
+
+
+
+
+// const thisGuy = new createObject("this", "dude", "rocks", "pasta")
+// console.log(thisGuy.p1)
+// console.log(thisGuy.p2)
+// console.log(thisGuy.p3)
+// console.log(thisGuy.tomatoes)
+// console.log(thisGuy.classFunc())
+
+// let thisObject = {
+//     dis: "this",
+    
+//     }
+//     console.log(thisObject.dis)
+    
+    
+//     thisObject.dis = "that"
+//     console.log(thisObject.dis)
